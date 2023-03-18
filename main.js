9cf7d5778ba39dd66aee902ef6a79199fae1d86b
DoTMInDs <123625827+DoTMInDs@users.noreply.github.com>
2023-03-19 01:46:32 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+menu = document.querySelector('.menu-bar');
+menu.onclick = function() {
+list = document.querySelector('.nav-top-middle');
+list.classList.toggle("active");
+}
+
+
+// function displayMenu(){
+//     var menu = document.getElementById("menu");
+//     menu.classList.toggle("navbar")
+// }
+// window.onclick = function(event){
+//     if(!event.target.matches("#navbar")){
+
+//         var menu = document.getElementById("menu")
+//          if (menu.classList.contains("active")){
+//             menu.classList.remove("active")
+//          }
+     
+//     }
+// }
+
+// window.onclick = function(event){
+//     if(!event.target.matches("#nav")){
+
+//         var menu = document.getElementById("menu")
+//         if (menu.classList.contains("active"))
+//         {
+//             remove.classList.remove("active")
+//         }
+//     }
+// }
+
+// const toggleBtn = document.querySelector('.drop-down-list')
+// const toggleBtnicon = document.querySelector('.drop-down-list i')
+// const togglePhone = document.querySelector('.phone')
+
+// toggleBtn.onclick = function () {
+//     togglePhone.classList.toggle('open')
+// }
+
+function dropDownList(){
+    var divs = document.getElementById("open");
+    divs.classList.toggle("show");
+}
+    
+
+window.onclick = function(event){
+    if(!event.target.matches("#display")){
+            
+        var removeshow = document.getElementById("open")
+        if (removeshow.classList.contains("show"))
+        {
+            removeshow.classList.remove("show")
+
+        }
+        }
+    }
+
+function dropDownService(){
+    var down = document.getElementById("get");
+    down.classList.toggle("aquire");
+}
+
+window.onclick = function(event){
+    if(!event.target.matches("#win")){
+
+        var take = document.getElementById("get")
+        if(take.classList.contains("aquire")){
+            take.classList.remove("aquire")
+        }
+    }
+}
