9f876a592b8aa92cc4434b6190ef8600578845d4
D•M•InDz <123625827+DoTMInDs@users.noreply.github.com>
2023-10-06 12:36:39 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -10,81 +10,37 @@ function closemenu(){
 }
 
 
+// -------------------------------learn-more-container-------------------//
+const learnMoreBtn = document.querySelector(".learnMore");
+const dropMore = document.querySelector(".learn-more-container");
+const backBtn = document.querySelector(".back-btn");
+
+learnMoreBtn.addEventListener('click', () => {
+    dropMore.classList.toggle('dropMoreContainer')
+
+    backBtn.addEventListener('click', () => {
+        dropMore.classList.remove('dropMoreContainer')
+    })
+})
 
 
-
-
-
-// menu = document.querySelector('.menu-bar');
-// menu.onclick = function() {
-// nav = document.querySelector('navbar');
-// nav.classList.toggle("active");
-// }
-
-
-// function displayMenu(){
-//     var menu = document.getElementById("menu");
-//     menu.classList.toggle("navbar")
-// }
-// window.onclick = function(event){
-//     if(!event.target.matches("#navbar")){
-
-//         var menu = document.getElementById("menu")
-//          if (menu.classList.contains("active")){
-//             menu.classList.remove("active")
-//          }
-     
-//     }
-// }
-
-// window.onclick = function(event){
-//     if(!event.target.matches("#nav")){
-
-//         var menu = document.getElementById("menu")
-//         if (menu.classList.contains("active"))
-//         {
-//             remove.classList.remove("active")
-//         }
-//     }
-// }
-
-// const toggleBtn = document.querySelector('.drop-down-list')
-// const toggleBtnicon = document.querySelector('.drop-down-list i')
-// const togglePhone = document.querySelector('.phone')
-
-// toggleBtn.onclick = function () {
-//     togglePhone.classList.toggle('open')
-// }
-
-function dropDownList(){
-    var divs = document.getElementById("open");
-    divs.classList.toggle("show");
-}
+// -----------------------------------swipper----------------------------//
+const slide = document.querySelector(".slide"),
+    reviews = document.querySelectorAll(".reviews"),
+    arrowIcons = document.querySelectorAll(".arrowBox i");
     
 
-window.onclick = function(event){
-    if(!event.target.matches("#display")){
-            
-        var removeshow = document.getElementById("open")
-        if (removeshow.classList.contains("show"))
-        {
-            removeshow.classList.remove("show")
 
-        }
-        }
+
+// ----------------------dark-mode-section----------------//
+var darkMode = document.getElementById('darkMode');
+
+darkMode.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        darkMode.style.color = 'white';
     }
-
-function dropDownService(){
-    var down = document.getElementById("get");
-    down.classList.toggle("aquire");
-}
-
-window.onclick = function(event){
-    if(!event.target.matches("#win")){
-
-        var take = document.getElementById("get")
-        if(take.classList.contains("aquire")){
-            take.classList.remove("aquire")
-        }
+    else {
+        darkMode.style.color = '#a78209';
     }
 }
